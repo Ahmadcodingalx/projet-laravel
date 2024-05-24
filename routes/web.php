65fd2b369b54dba1ee5dc6526296aac9f63641b8
= <=>
2024-05-24 10:56:04 +0000

    update8

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
+
+Route::get('/products', [App\Http\Controllers\ProductController::class, 'index'])->name("product_list");
